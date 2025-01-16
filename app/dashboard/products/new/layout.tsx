c98d7198dd84c01cc83084b767ed5e1836bf0d7b
Boris <adrboris@outlook.com>
2025-01-16 17:37:57 +0300

    load

--- app/dashboard/products/new/layout.tsx
+++ app/dashboard/products/new/layout.tsx
@@ -1,15 +1,13 @@
-import { Suspense } from "react"
-import { PageTitle } from "../../_components/PageTitle"
 
-const NewProductLayout = ({children}: Readonly<{children: React.ReactNode}>) => {
-    return (
-        <>
-        <PageTitle>Новый продукт</PageTitle>
-        <Suspense fallback={<p>Load...</p>}>
-            {children}
-        </Suspense>
-        </>
-    )
-}
+import { PageTitle } from '../../_components/PageTitle';
 
-export default NewProductLayout
+const NewProductLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <>
+      <PageTitle>Новый продукт</PageTitle>
+      {children}
+    </>
+  );
+};
+
+export default NewProductLayout;
